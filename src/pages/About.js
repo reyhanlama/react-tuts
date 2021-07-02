a7ee8c8f256969232079eebd2495d8a957a14237
reyhantamang@gmail.com <reyhantamang@gmail.com>
2021-07-02 13:02:43 +0530

    initial

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us </h1>
+      <p>
+        Irure esse eu nisi ipsum ex aliqua amet et anim ea qui occaecat officia.
+        Dolor Lorem elit incididunt exercitation ipsum aliquip voluptate
+        incididunt et aute ea tempor. Culpa in culpa ex officia cupidatat. Enim
+        exercitation excepteur eiusmod reprehenderit ex exercitation incididunt
+        fugiat culpa est. Eu aliqua est cupidatat sint Lorem exercitation tempor
+        nostrud. Id dolore sint adipisicing aute irure minim proident ex culpa.
+        Culpa nulla adipisicing veniam laboris non.
+      </p>
+    </section>
+  )
+}
+
+export default About
